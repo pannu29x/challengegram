8b62176dcc9d69712504543ec2be1deea4e55998
pannu29x <pranayraut5022@gmail.com>
2025-08-15 01:09:32 +0530

    Delete db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-import pkg from 'pg';
-const { Pool } = pkg;
-import dotenv from 'dotenv';
-dotenv.config();
-export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL && !process.env.DATABASE_URL.includes('localhost') ? { rejectUnauthorized: false } : false
-});
